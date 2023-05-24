cb95b1cc4dff52757f20f1fee897fb5e04eae3d6
Jorge-Gil <jorge.gil@uao.edu.co>
2023-05-23 19:34:19 -0500

    creación, actualización y eliminación de Comentarios, palabras clave y usuario

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,17 @@
-import express from 'express';
-import {  login } from '../controllers/auth.js';
+import express from "express";
+import { login } from "../controllers/auth.js";
+import { crearComentario } from "../controllers/auth.js";
+import { updateUsuario } from "../controllers/auth.js";
+import { deleteUsuario } from "../controllers/auth.js";
+import { deletecomentario } from "../controllers/auth.js";
 
 const router = express.Router();
 
-router.post('/login', login);
+router.post("/login", login);
+router.post("/comentar", crearComentario);
+router.put("/updateUsuario/:usuarioId", updateUsuario);
+router.delete("/deleteUsuario/:usuarioId", deleteUsuario);
+router.delete("/deleteComentario/:comentarioId", deletecomentario);
+
 
 export default router;
